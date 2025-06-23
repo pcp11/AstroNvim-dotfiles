9519c555288823e4b18f160fd6a146ccf454f7cb
pcp11 <patrick.christ@protonmail.com>
2025-06-23 23:38:37 +0200

    add plugins

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,34 @@
+-- Customize Mason
+
+---@type LazySpec
+return {
+  -- use mason-tool-installer for automatically installing Mason packages
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    -- overrides `require("mason-tool-installer").setup(...)`
+    opts = {
+      -- Make sure to use the names found in `:Mason`
+      ensure_installed = {
+        -- install language servers
+        "lua-language-server",
+
+        -- install formatters
+        "stylua",
+
+        -- install debuggers
+        "debugpy",
+
+        -- install any other package
+        "tree-sitter-cli",
+
+        "typescript-language-server",
+
+        "vue-language-server",
+
+        "csharp-language-server",
+
+        "angular-language-server",
+      },
+    },
+  },
+}
